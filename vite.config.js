ad6a1986ccb4cda5d76e76f4698260e566836319
mrstraw-hat <muhammedfalah90@gmail.com>
2024-11-09 23:51:29 +0530

    Updated dependencies and package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/muhammedfalah/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  }
 })
